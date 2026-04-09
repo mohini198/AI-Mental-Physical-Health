8d2f2b41166065dfd1c15338482bbc4e03367369
mohini198 <chauhanmohini198@gmail.com>
2026-04-09 14:16:28 +0530

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 from torchvision import models
 
 def get_model():
-    model = models.densenet121(pretrained=True)
+    model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
+
     num_features = model.classifier.in_features
     model.classifier = nn.Linear(num_features, 2)
+
     return model
-    
